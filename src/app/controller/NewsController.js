19aef669a5401bad870dd06e3d111013343f0764
tran tien <tranbon010702@gmail.com>
2024-07-27 11:12:52 +0700

    add new-news

--- src/app/controller/NewsController.js
+++ src/app/controller/NewsController.js
@@ -1,6 +1,8 @@
 const News = require('../model/News');
 const sequelize = require('../../config/db');
 const { resultsToObject, resultToObject, addProp } = require('../../util/sequelize');
+const { Op } = require('sequelize');
+
 class NewsController {
     async index(req, res) {
         try {
@@ -36,6 +38,20 @@ class NewsController {
         try {
             let data;
             let newsDetail;
+            let newNews;
+            newNews = await News(sequelize).findAll({ limit: 7, where: {
+                published_date: {[Op.gte]: new Date(new Date() - 7 * 24 * 60 * 60 * 1000) }
+            }, order: [['published_date', 'DESC']] });
+            newNews = await Promise.all(newNews.map(
+                async (item, index) => {
+                    let thumbnail = await item.getImages();
+                    thumbnail = thumbnail.find(
+                        (img)=>img.position === 'thumbnail'
+                    );
+                    return ({ ...resultToObject(item, 'model'), ...thumbnail });
+                }
+            ));
+
             data = await News(sequelize).findOne({ where: { slug: req.params.slug } });
             let imgs = await data.getImages();
             imgs = imgs.reduce((newImgs, img, index) => {
@@ -44,12 +60,12 @@ class NewsController {
                 else
                     return {...newImgs, [`content${index}`]: img.image_url};
             }, {})
-            newsDetail = {...resultToObject(data, 'model'), ...imgs}
-            // newsDetail = resultsToObject(img);
-            // newsDetail = { ...data, ...newsDetail };
+            newsDetail = {...resultToObject(data, 'model'), ...imgs};
+            console.log(newNews);
             res.render('news/news-detail',
                 {
-                    newsDetail: [newsDetail],
+                    newNews: newNews,
+                    newsDetail: newsDetail,
                     noSlider: true,
                     cssFiles: ['newsDetail.css', 'news.css']
                 });
